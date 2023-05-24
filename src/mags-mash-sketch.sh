ffb1a623d899e58c679ca8fee8e91f550d1035f4
Luiggi Tenorio Ku <luiggi.tenorio@pucp.pe>
2023-05-24 17:00:22 +0200

    add omh k21 l1

--- src/mags-mash-sketch.sh
+++ src/mags-mash-sketch.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+K=21
+S=1000
+S_PARAMS="k${K}-s${S}"
+
 date1=$(date +"%s")
 echo "Started " "$(date)"
 
-mash sketch -p 4 -o sketches/mags-mash-k21-s1000.fa.msh -i ./data/mags_concat.fa
+mash sketch -p 4 -o "sketches/mags-mash-${S_PARAMS}.fa.msh" -i ./data/mags_concat.fa
 
 date2=$(date +"%s")
 DIFF=$((date2-date1))
@@ -11,4 +15,4 @@ echo "Duration: $((DIFF / 3600 )) hours $(((DIFF % 3600) / 60)) minutes $((DIFF 
 
 # Last run 1 sec
 
-mash dist -p 4 sketches/mags-mash-k21-s1000.fa.msh sketches/mags-mash-k21-s1000.fa.msh > distances/mags-mash-k21-s1000.dist
+mash dist -p 4 "sketches/mags-mash-${S_PARAMS}.fa.msh" "sketches/mags-mash-${S_PARAMS}.fa.msh" > "distances/mags-mash-${S_PARAMS}.dist"
--- src/mags-omh-sketch.sh
+++ src/mags-omh-sketch.sh
@@ -1,14 +1,69 @@
 #!/bin/bash
 
+# Set 1. Last run: 37 minutes 14 seconds
+# S_PARAMS=k5-l2-m1000
+
+# date1=$(date +"%s")
+# echo "Started " "$(date)"
+
+# omh_sketch -o "sketches/mags-omh-${S_PARAMS}.fa.omh" ./data/mags_concat.fa
+
+# date2=$(date +"%s")
+# DIFF=$((date2-date1))
+# echo "Duration: $((DIFF / 3600 )) hours $(((DIFF % 3600) / 60)) minutes $((DIFF % 60)) seconds"
+
+# omh_compare_sketches "sketches/mags-omh-${S_PARAMS}.fa.omh" "sketches/mags-omh-${S_PARAMS}.fa.omh" > "distances/mags-omh-${S_PARAMS}.dist"
+
+
+# Set 2. Last run: 41 minutes 39 seconds
+K=21
+L=1
+M=1000
+S_PARAMS="k${K}-l${L}-m${M}"
+
 date1=$(date +"%s")
 echo "Started " "$(date)"
 
-omh_sketch -o sketches/mags-omh-k5-l2-m1000.fa.omh ./data/mags_concat.fa
+omh_sketch -k "${K}" -l "${L}" -m "${M}" -o "sketches/mags-omh-${S_PARAMS}.fa.omh" ./data/mags_concat.fa
 
 date2=$(date +"%s")
 DIFF=$((date2-date1))
 echo "Duration: $((DIFF / 3600 )) hours $(((DIFF % 3600) / 60)) minutes $((DIFF % 60)) seconds"
 
-# Last run: 37 min 14 sec
+omh_compare_sketches "sketches/mags-omh-${S_PARAMS}.fa.omh" "sketches/mags-omh-${S_PARAMS}.fa.omh" > "distances/mags-omh-${S_PARAMS}.dist"
 
-omh_compare_sketches sketches/mags-omh-k5-l2-m1000.fa.omh sketches/mags-omh-k5-l2-m1000.fa.omh > distances/mags-omh-k5-l2-m1000.dist
+
+# # Set 3. Last run: 
+# K=21
+# L=2
+# M=1000
+# S_PARAMS="k${K}-l${L}-m${M}"
+
+# date1=$(date +"%s")
+# echo "Started " "$(date)"
+
+# omh_sketch -k "${K}" -l "${L}" -m "${M}" -o "sketches/mags-omh-${S_PARAMS}.fa.omh" ./data/mags_concat.fa
+
+# date2=$(date +"%s")
+# DIFF=$((date2-date1))
+# echo "Duration: $((DIFF / 3600 )) hours $(((DIFF % 3600) / 60)) minutes $((DIFF % 60)) seconds"
+
+# omh_compare_sketches "sketches/mags-omh-${S_PARAMS}.fa.omh" "sketches/mags-omh-${S_PARAMS}.fa.omh" > "distances/mags-omh-${S_PARAMS}.dist"
+
+
+# # Set 4. Last run: 
+# K=12
+# L=2
+# M=1000
+# S_PARAMS="k${K}-l${L}-m${M}"
+
+# date1=$(date +"%s")
+# echo "Started " "$(date)"
+
+# omh_sketch -k "${K}" -l "${L}" -m "${M}" -o "sketches/mags-omh-${S_PARAMS}.fa.omh" ./data/mags_concat.fa
+
+# date2=$(date +"%s")
+# DIFF=$((date2-date1))
+# echo "Duration: $((DIFF / 3600 )) hours $(((DIFF % 3600) / 60)) minutes $((DIFF % 60)) seconds"
+
+# omh_compare_sketches "sketches/mags-omh-${S_PARAMS}.fa.omh" "sketches/mags-omh-${S_PARAMS}.fa.omh" > "distances/mags-omh-${S_PARAMS}.dist"
